995c5441d68d07882b82b3ad8e9292b304b2d149
GloryDream413 <glorydream413@gmail.com>
2025-11-06 21:56:13 +0100

    Compile error solved.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'us-west-2.graphassets.com',
+        pathname: '/**',
+      },
+    ],
     domains: [
       'media.graphassets.com',
       'res.cloudinary.com',
